84500c34541b0ab03e4ee6c86b22f46508c03bc7
Jan Poonthong <janpoonthong628@gmail.com>
2021-05-09 11:13:55 +0700

    Add background-color to button when it's click

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function start() {
   // TODO(jan): Remove box-shawdow when start button is press
+  console.log("start");
+  // TODO(jan): Doesn't add to id, instead it create new class
+  document.querySelector("#start").classList.add("button-start-click");
+}
+
+function pomodoroTimer() {
+  console.log("pomodoro");
+  document.querySelector(".timer").classList.add("timer-button-click");
+}
+
+function breakTimer() {
+  // TODO(jan): When break button is click the background-color appear on Pomodoro
+  // TODO(jan): select query using Id but add class
+  console.log("break");
+  document.querySelector(".timer").classList.add("timer-button-click");
 }
