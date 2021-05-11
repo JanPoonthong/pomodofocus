b907c7f4e2b248569a1bb1edf7f15ee8f0f41f86
Jan Poonthong <janpoonthong628@gmail.com>
2021-05-11 10:27:24 +0700

    Change the text when the start button is clicked a second time

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+let clicked = false;
 function start() {
   document.querySelector(".start").classList.add("button-start-click");
   document.querySelector(".start").innerHTML = "STOP";
+  if (clicked) {
+    document.querySelector(".start").className = "start";
+    document.querySelector(".start").innerHTML = "START";
+  }
+  clicked = !clicked
 }
 
 function pomodoroTimer() {
