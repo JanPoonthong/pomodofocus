f45c26fbbe2ec855058e5a25cb29774f7deaf727
Jan Poonthong <janpoonthong628@gmail.com>
2021-05-13 09:41:47 +0700

    Remove unuse functions

--- index.js
+++ index.js
@@ -9,15 +9,3 @@ function start() {
   }
   clicked = !clicked;
 }
-
-function pomodoroTimer() {
-  document.querySelector(".timer-break").className = "timer-break";
-  document.querySelector(".timer-pomodoro").className =
-    "timer-pomodoro-default";
-}
-
-function breakTimer() {
-  document.querySelector(".timer-pomodoro-default").className =
-    "timer-pomodoro";
-  document.querySelector(".timer-break").classList.add("timer-button-click");
-}
